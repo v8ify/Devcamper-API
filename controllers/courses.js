0a9574de5cfc7676bec6217bb413c822a92bf77e
Prajwal-Jadhav <jadhavv933@gmail.com>
2020-07-02 12:09:12 +0530

    Add and get a specific course

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,6 +1,7 @@
 const Course = require("../models/Course");
 const geocoder = require("../utils/geocoder");
 const errorResponse = require("../utils/ErrorResponse");
+const Bootcamp = require("../models/Bootcamp");
 
 // @desc      Get courses
 // @route     GET /api/v1/courses
@@ -20,6 +21,49 @@ exports.getCourses = async (req, res, next) => {
     res
       .status(200)
       .json({ success: true, count: courses.length, data: courses });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// @desc      Get single courses by id
+// @route     GET /api/v1/courses/:id
+// @access    Public
+exports.getCourse = async (req, res, next) => {
+  try {
+    const course = await Course.findById(req.params.id);
+
+    if (!course) {
+      return next(
+        new errorResponse(`Couldn't find course with id ${req.params.id}`, 404)
+      );
+    }
+
+    res.status(200).json({ success: true, data: course });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// @desc      Add single course by bootcampId
+// @route     POST /api/v1/bootcamp/:bootcampId/courses
+// @access    Private
+exports.createCourse = async (req, res, next) => {
+  req.body.bootcamp = req.params.bootcampId;
+
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+
+    if (!bootcamp) {
+      throw new errorResponse(
+        `Couldn't find bootcamp with id ${req.params.bootcampId}`,
+        400
+      );
+    }
+
+    const course = await Course.create(req.body);
+
+    res.status(200).json({ success: true, data: course });
   } catch (error) {
     next(error);
   }
--- routes/courses.js
+++ routes/courses.js
@@ -1,8 +1,14 @@
 const express = require("express");
-const { getCourses } = require("../controllers/courses");
+const {
+  getCourses,
+  getCourse,
+  createCourse,
+} = require("../controllers/courses");
 
 const router = express.Router({ mergeParams: true });
 
-router.route("/").get(getCourses);
+router.route("/").get(getCourses).post(createCourse);
+
+router.route("/:id").get(getCourse);
 
 module.exports = router;
